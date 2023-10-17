104aaa001dde7672ac8f00dae7edb779ed4f3d9d
Afr Schoe <58883403+q9f@users.noreply.github.com>
2023-10-17 15:54:21 +0200

    remove promises to retain order

--- create-image-json.js
+++ create-image-json.js
@@ -7,50 +7,26 @@
 const fs = require('fs');
 const sizeOf = require('image-size');
 
-fs.readdir(__dirname, (err, files) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+fs.readdir(__dirname, (_, files) => {
     let i = 1;
-    let promises = [];
     let jsonFiles = [];
     let images = files.filter((fileName) => {
         let extension = fileName.substr(fileName.lastIndexOf('.')+1);
         return extension === 'jpg';
     });
     images.forEach((fileName) => {
-        let promise = new Promise((resolve, reject) => {
-            fs.stat(fileName, (stat_err, _st) => {
-                if (stat_err) {
-                    reject(stat_err);
-                    return;
-                }
-                let size_err, dimensions = sizeOf(fileName);
-                if (size_err) {
-                    reject(size_err);
-                    return;
-                }
-                let image = {
-                    filenumber: i,
-                    name: fileName,
-                    width: dimensions.width,
-                    height: dimensions.height
-                };
-                jsonFiles.push(image);
-                i++;
-                resolve();
-            });
-        });
-        promises.push(promise);
+        let _, dimensions = sizeOf(fileName);
+        let image = {
+            filenumber: i,
+            name: fileName,
+            width: dimensions.width,
+            height: dimensions.height
+        };
+        jsonFiles.push(image);
+        i++;
     });
 
-    Promise.all(promises).then(() => {
-        fs.writeFile('images.json', JSON.stringify(jsonFiles), (err) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log(`Found ${jsonFiles.length} items. Created ${__dirname + '/images.json' }. All done!`);
-        });
+    fs.writeFile('images.json', JSON.stringify(jsonFiles), (_) => {
+        console.log(`Found ${jsonFiles.length} items. Created ${__dirname + '/images.json' }. All done!`);
     });
 })
